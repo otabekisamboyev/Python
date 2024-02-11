d7753ff652fa5a8e99b543f9b2a33b8bd1a983db
Otabek <154885598+youdonotgetmysoul@users.noreply.github.com>
2024-02-11 15:32:41 +0500

    main

--- main.py
+++
@@ -1,6 +0,0 @@
-print("Welcome to the tip calculator.")
-bill = float(input("What was the total bill? $"))
-people = int(input("How many people to split the bill? "))
-tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
-pay = (bill + (bill * tip)/100) / people
-print(f"Each person should pay: {pay}")
